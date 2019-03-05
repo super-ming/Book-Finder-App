94bfdc0e8e3462c8394f48cfda3d61631f68102c
Ming Ho <hkboi108@hotmail.com>
2019-03-04 18:24:38 -0800

    Updates

---
+++ precache-manifest.1ce56cb932b63b6df05e5933c7bc741c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee052b777c1b3ee7a11",
+    "url": "/Book-Finder-App/static/js/runtime~main.fee052b7.js"
+  },
+  {
+    "revision": "5e570deb8a90d7b98916",
+    "url": "/Book-Finder-App/static/js/main.5e570deb.chunk.js"
+  },
+  {
+    "revision": "ebf017cb95b5611b52fd",
+    "url": "/Book-Finder-App/static/js/2.ebf017cb.chunk.js"
+  },
+  {
+    "revision": "5e570deb8a90d7b98916",
+    "url": "/Book-Finder-App/static/css/main.5e3d9351.chunk.css"
+  },
+  {
+    "revision": "0cc89181d0c65a6e44f6e3e719810872",
+    "url": "/Book-Finder-App/index.html"
+  }
+];
